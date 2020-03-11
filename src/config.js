2b5fe42d1b2d35064e9db3cbb0869f47ac598d1e
Fernando Gaol <ffilho@riolindo.org>
2020-03-11 00:30:54 -0600

    first commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+  API_ENDPOINT: 'http://localhost:8000/api',
+  TOKEN_KEY: 'thingful-client-auth-token',
+}
+
+//filter through the existing usernames in my dabatase 
+//passsword cant be white spaces, cant be less than 8 character or longer than 72 chars
+//regex for password check
